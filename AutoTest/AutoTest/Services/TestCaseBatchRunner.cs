85bb674555c97f0b207962283f7ab656b4f6c42b
Jason <yangqb@moa.com.au>
2026-05-28 18:01:55 +0800

    no message

--- AutoTest/AutoTest/Services/TestCaseBatchRunner.cs
+++ AutoTest/AutoTest/Services/TestCaseBatchRunner.cs
@@ -51,6 +51,7 @@ public sealed class TestCaseBatchRunner
             {
                 var a = 1;
                 var b = 1;
+                var c = 1;
                 var contextOptions = new BrowserNewContextOptions { IgnoreHTTPSErrors = true };
                 if (options.EnableVideoOnFailure)
                 {
